a47331ff6e24d0683c10f528ee288beac33f18ac
concept2work <48356445+concept2work@users.noreply.github.com>
2021-03-18 11:57:18 +0100

    Corrected SASS variables

--- src/server/index.js
+++ src/server/index.js
@@ -39,7 +39,13 @@ const apiURI = 'api.meaningcloud.com/sentiment-2.1';
 // The home page route
 app.get('/', (req, res) => {
   res.sendStatus(200);
-  res.sendFile('dist/index.html');
+  if (process.env.NODE_ENV === 'production') {
+    res.sendFile('dist/index.html');
+  }
+  if (process.env.NODE_ENV === 'development') {
+    res.sendFile('dist/index.html');
+    // res.sendFile(path.resolve(__dirname, '.../client//views/index.html'));
+  }
 });
 
 // Function and route to handle the provided URL from the client.
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,20 @@
 const { merge } = require('webpack-merge');
 const dotenv = require('dotenv').config({ path: './.env' });
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
   devServer: {
+    contentBase: './dist',
     historyApiFallback: true,
     hot: true,
     inline: true,
     port: process.env.PORT_DEV,
     proxy: {
-      '/api': `http://localhost:${process.env.PORT_DEV_PROXY}`,
+      '*': `http://[::1]:${process.env.PORT_DEV_PROXY}`,
+      // '/api': `http://localhost:${process.env.PORT_DEV_PROXY}`,
     },
   },
   module: {
@@ -22,4 +25,10 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+  ],
 });
